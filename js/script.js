b77b00ff5d9f96bcd76efa327f590891ad072f82
megandye248 <dyexx077@umn.edu>
2016-03-09 11:20:16 -0600

    Birds

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
+
+	$(function() {  
+		$('.greenBox1').click(function(){
+    	$(this).css('background', '#30e964');
+
+  		});
+	});
+	
+	$(function() {  
+		$('.greenBox2').click(function(){
+    	$(this).css('background', 'red');
+
+  		});
+	});
+	
+	$(function() {  
+		$('.greenBox3').click(function(){
+    	$(this).css('background', 'red');
+
+  		});
+	});
+	
+	$(function() {  
+		$('.greenBox4').click(function(){
+    	$(this).css('background', 'red');
+
+  		});
+	});
+	
+	var audio1 = document.createElement('audio');
+		audio1.setAttribute('src', 'sounds/1.wav');
+		
+	$('#sound1').click(function(){
+		audio1.play();
+	});
 	
 });
